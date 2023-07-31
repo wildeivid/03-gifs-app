42519a2166f6985342035112b7a08a82a19677ba
Wilson Cordero <wilson.cordero@itac.co>
2023-07-31 16:51:50 -0500

    update

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,15 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar:</h5>
+    <input type="text" class="form-control" placeholder="Buscar Gifs...">
+  `
+})
+
+export class SearchBoxComponent {
+  constructor() { }
+
+}
